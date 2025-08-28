e1a0a6ef986c5cbde614e9825db286b0cdf9f319
Stanley57-dev <Chikastanley464@gmail.com>
2025-08-28 07:46:28 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// let box = document.getElementById('box');
+// let pos = 0;
+
+// function move() {
+//     if (pos < 300) {
+//         pos++
+//         box.style.left = pos + 'px';
+//         requestAnimationFrame(move)
+//     }
+// } move()
+
+ document.getElementById("btn").addEventListener("click", function() {
+
+alert("😂you can't catch me!");
+  })
+
